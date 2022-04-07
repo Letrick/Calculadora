8df520e965aec806e8c72e4cea148e8eaef82ccd
Patrick Leal <80593781+Letrick@users.noreply.github.com>
2022-04-07 18:22:48 -0300

    Add files via upload

---
+++ multiplicacao.java
@@ -0,0 +1,53 @@
+
+public class multiplicacao implements ICalculadora{
+
+	public multiplicacao(int n1, int n2) {
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	@Override
+	public Integer calcula(Integer n1, Integer n2) {
+		
+		float resultado = n1 * n2;
+		
+		
+		return null;
+	}
+
+	@Override
+	public Float calcula(Float n1, Float n2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double calcula(Double n1, Double n2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer calcula(Integer[] numeros) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Float calcula(Float[] numeros) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double calcula(Double[] numeros) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

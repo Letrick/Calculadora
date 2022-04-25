0e52af7a156a621b50b0ab7bb13abb9bbadab946
Patrick Leal <80593781+Letrick@users.noreply.github.com>
2022-04-25 09:50:47 -0300

    Add files via upload

--- ICalculadora.java
+++ ICalculadora.java
@@ -1,13 +1,18 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
 
-public interface ICalculadora {
-	
-	
-	Integer calcula(Integer n1, Integer n2);
-	Float calcula(Float n1, Float n2);
-	Double calcula(Double n1, Double n2);
-	
-	Integer calcula(Integer[] numeros);
-	Float calcula(Float[] numeros);
-	Double calcula(Double[] numeros);
-
+public interface ICalculadora
+{
+    Integer calcula(final Integer p0, final Integer p1);
+    
+    Float calcula(final Float p0, final Float p1);
+    
+    Double calcula(final Double p0, final Double p1);
+    
+    Integer calcula(final Integer[] p0);
+    
+    Float calcula(final Float[] p0);
+    
+    Double calcula(final Double[] p0);
 }
--- multiplicacao.java
+++ multiplicacao.java
@@ -1,53 +1,48 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
 
-public class multiplicacao implements ICalculadora{
-
-	public multiplicacao(int n1, int n2) {
-		
-		
-		
-		
-		
-		
-		
-	}
-
-	@Override
-	public Integer calcula(Integer n1, Integer n2) {
-		
-		float resultado = n1 * n2;
-		
-		
-		return null;
-	}
-
-	@Override
-	public Float calcula(Float n1, Float n2) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Double calcula(Double n1, Double n2) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Integer calcula(Integer[] numeros) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Float calcula(Float[] numeros) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Double calcula(Double[] numeros) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+public class multiplicacao implements ICalculadora
+{
+    @Override
+    public Integer calcula(final Integer n1, final Integer n2) {
+        return n1 * n2;
+    }
+    
+    @Override
+    public Float calcula(final Float n1, final Float n2) {
+        return n1 * n2;
+    }
+    
+    @Override
+    public Double calcula(final Double n1, final Double n2) {
+        return n1 * n2;
+    }
+    
+    @Override
+    public Integer calcula(final Integer[] numeros) {
+        Integer mult = 0;
+        for (int i = 0; i < numeros.length; ++i) {
+            mult *= numeros[i];
+        }
+        return mult;
+    }
+    
+    @Override
+    public Float calcula(final Float[] numeros) {
+        Float mult = 0.0f;
+        for (int i = 0; i < numeros.length; ++i) {
+            mult *= numeros[i];
+        }
+        return mult;
+    }
+    
+    @Override
+    public Double calcula(final Double[] numeros) {
+        Double mult = 0.0;
+        for (int i = 0; i < numeros.length; ++i) {
+            mult *= numeros[i];
+        }
+        return mult;
+    }
 }
